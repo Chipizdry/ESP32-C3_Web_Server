b259932e371d7018a62f1d4ab578d416e0ecee47
Chipizdry <79665980+Chipizdry@users.noreply.github.com>
2024-10-22 16:48:31 +0300

    22.10.2024_Разбивка на библиотеки

--- Web_Server/main/main.c
+++ Web_Server/main/main.c
@@ -29,7 +29,7 @@
 #include <time.h>
 #include "lwip/ip4_addr.h"
 #include "soc/gpio_num.h"
-
+#include "uart.h"
 
 // Статический IP и параметры Wi-Fi AP
 #define AP_SSID_PREFIX "FlyWheel-"  // Префикс для SSID
@@ -55,19 +55,14 @@
 #define UART_BAUD_RATE     57600
 #define UART_BUF_SIZE      (1024)
 #define RX_BUFFER_SIZE 32
-#define HEADER_1 0x55
-#define HEADER_2 0xAA
+
 
 #define RESET_PIN GPIO_NUM_0  // Выберите нужный пин, например, GPIO 0
-// Определяем командные коды
-#define COMMAND_REGULAR_REQUEST 0x01  // Команда для регулярного запроса данных
-#define COMMAND_FRONTEND 0x02         // Команда от фронтэнда
+
 // Размер очереди
-#define QUEUE_SIZE 10
+//#define QUEUE_SIZE 10
 httpd_handle_t server_handle = NULL;
 
-// Основная функция приложения
-//static bool nvs_initialized = false;
 static bool netif_initialized = false;
 static bool event_loop_created = false;
 static bool filesystem_mounted = false;
@@ -112,13 +107,6 @@ device_settings_t default_settings = {
     .ap_netmask = "255.255.255.0",
     .ap_gateway = "192.168.1.1"
 };
-// Структура для передачи данных через очередь
-typedef struct {
-	uint8_t cmd_type;
-    uint8_t command;
-    uint8_t payload[10];  // Полезная нагрузка, максимальная длина 10 байт
-    uint8_t payload_len;
-} uart_command_t;
 
 
 // Структура для данных
@@ -131,17 +119,15 @@ typedef struct {
     int signal_strength; // Сигнал (RSSI)
 } sensor_data_t;
 
-static QueueHandle_t uart_command_queue;
-//static QueueHandle_t uart_event_queue;
-static QueueHandle_t uart_queue;
+//static QueueHandle_t uart_command_queue;
+
 // Объявляем мьютекс
 SemaphoreHandle_t uart_mutex;
 
 uint8_t rx_buffer[RX_BUFFER_SIZE];
-// Прототипы функций
-void process_received_data(uint8_t *data, int len);
 void reset_to_factory_settings();
 void init_wifi_ap();
+
 // Инициализация пина для сброса
 void init_reset_pin() {
     gpio_config_t io_conf = {};
@@ -153,38 +139,7 @@ void init_reset_pin() {
 }
 
 
-static void IRAM_ATTR uart_isr(void* arg) {
-    // Код, который будет выполняться в ISR
-}
 
-void init_uart() {
-    const uart_port_t uart_num = UART_NUM_1;
-
-    // Настройки UART
-    uart_config_t uart_config = {
-        .baud_rate = UART_BAUD_RATE,
-        .data_bits = UART_DATA_8_BITS,
-        .parity    = UART_PARITY_DISABLE,
-        .stop_bits = UART_STOP_BITS_1,
-        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
-        .source_clk = UART_SCLK_APB,
-    };
-
-    uart_param_config(uart_num, &uart_config);
-    uart_set_pin(uart_num, TXD_PIN, RXD_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
-
-    // Инициализация UART с использованием DMA
-   // esp_err_t uart_err = uart_driver_install(UART_NUM_1, UART_BUF_SIZE, UART_BUF_SIZE, 0, &uart_queue, 0);
-    esp_err_t uart_err =  uart_driver_install(UART_NUM_1,UART_BUF_SIZE * 2, UART_BUF_SIZE* 2, 100, &uart_queue, ESP_INTR_FLAG_IRAM);
-    if (uart_err != ESP_OK) {
-        ESP_LOGE(TAG, "Failed to install UART driver: %s", esp_err_to_name(uart_err));
-    }
-
-     // Настройка паттерна для детектирования
-    uart_enable_pattern_det_baud_intr(UART_NUM_1, '\n', 1, 9, 0, 0);  // Паттерн '\n'
-    uart_pattern_queue_reset(UART_NUM_1, 20);
-
-}
 
 // Задача для отслеживания состояния пина
 void reset_task(void *arg) {
@@ -202,210 +157,6 @@ void reset_task(void *arg) {
     }
 }
 
-
-
-
-uint16_t calcCRC16(uint8_t *buffer, uint8_t u8length) {
-	unsigned int temp, temp2, flag;
-	temp = 0xFFFF;
-	for (unsigned char i = 0; i < u8length; i++) {
-		temp = temp ^ buffer[i];
-		for (unsigned char j = 1; j <= 8; j++) {
-			flag = temp & 0x0001;
-			temp >>= 1;
-			if (flag)
-				temp ^= 0xA001;
-		}
-	}
-	// Reverse byte order.
-	temp2 = temp >> 8;
-	temp = (temp << 8) | temp2;
-	temp &= 0xFFFF;
-	// the returned value is already swapped
-	// crcLo byte is first & crcHi byte is last
-	return temp;
-
-}
-
-// Функция для формирования запроса
-uint16_t form_tuya_request(uint8_t cmd_type, uint8_t command, uint8_t *payload, uint16_t payload_len, uint8_t *request) {
-    uint16_t pos = 0;
-
-    // 1. Заголовок (2 байта)
-    request[pos++] = HEADER_1;
-    request[pos++] = HEADER_2;
-
-    // 2. Тип пакета (1 байт), 0x00 для запроса
-                    //         0x01 для команд
-                    //         0x02 для файлов прошивки
-              
-    request[pos++] =cmd_type;
-
-    // 3. Идентификатор команды (1 байт)
-    request[pos++] = command;
-
-    // 4. Длина данных (2 байта) — длина полезной нагрузки
-    request[pos++] = (payload_len >> 8) & 0xFF;  // Старший байт длины
-    request[pos++] = payload_len & 0xFF;         // Младший байт длины
-
-    // 5. Полезная нагрузка (N байт)
-    if (payload != NULL && payload_len > 0) {
-        memcpy(&request[pos], payload, payload_len);
-        pos += payload_len;
-    }
-
-   // 6. CRC (2 байта) — вычисляем CRC для всех предыдущих данных
-    uint16_t crc = calcCRC16(request, pos);
-    request[pos++] = crc & 0xFF;         // Младший байт CRC
-    request[pos++] = (crc >> 8) & 0xFF;  // Старший байт CRC
-
-    return pos;  // Возвращаем длину сформированного запроса
-}
-
-
-// Задача для обработки данных из UART
-static void uart_event_task(void *pvParameters) {
-    uart_event_t event;
-    size_t buffered_size;
-    uint8_t* data = (uint8_t*) malloc(UART_BUF_SIZE);
-    
-    while (1) {
-        // Ждём, когда придёт событие из UART
-        if (xQueueReceive(uart_queue, (void*)&event, (TickType_t)portMAX_DELAY)) {
-            bzero(data,UART_BUF_SIZE);
-
-            switch (event.type) {
-                // Данные пришли
-                case UART_DATA:
-                    uart_get_buffered_data_len(UART_NUM_1, &buffered_size);
-                    int len = uart_read_bytes(UART_NUM_1, data, event.size, pdMS_TO_TICKS(100));
-                    ESP_LOGI(TAG, "Received data of length: %d", len);
-                  
-                    process_received_data(data,len); 
-                    break;
-
-                // Ошибка фрейма
-                case UART_FRAME_ERR:
-                    ESP_LOGE(TAG, "UART Frame Error");
-                    break;
-
-                // Ошибка паритета
-                case UART_PARITY_ERR:
-                    ESP_LOGE(TAG, "UART Parity Error");
-                    break;
-
-                // Ошибка буфера
-                case UART_BUFFER_FULL:
-                    ESP_LOGW(TAG, "Ring buffer full");
-                    uart_flush_input(UART_NUM_1);  // Очищаем входной буфер
-                    xQueueReset(uart_queue);     // Сбрасываем очередь
-                    break;
-
-                // Таймаут
-                case UART_FIFO_OVF:
-                    ESP_LOGW(TAG, "UART FIFO Overflow");
-                    uart_flush_input(UART_NUM_1);  // Очищаем входной буфер
-                    xQueueReset(uart_queue);     // Сбрасываем очередь
-                    break;
-
-                // Событие по умолчанию
-                default:
-                    ESP_LOGI(TAG, "UART event type: %d", event.type);
-                    break;
-            }
-        }
-    }
-    free(data);
-    vTaskDelete(NULL);
-}
- 
- // Функция обработки принятых данных
-void process_received_data(uint8_t *data, int len) {
-  
-      ESP_LOG_BUFFER_HEX(TAG, data, len); 
-     if (len < 6) {
-        ESP_LOGW(TAG, "Packet too short");
-        return;
-    }
-    
-     // Проверяем заголовок
-    if (data[0] != HEADER_1 || data[1] != HEADER_2) {
-        ESP_LOGW(TAG, "Invalid header");
-        return;
-    }
-    
-      // Проверяем CRC
-    uint16_t crc_received = (data[len - 1] << 8) | data[len - 2];
-    uint16_t crc_calculated = calcCRC16(data,len - 2);
-    ESP_LOGI(TAG, "Received CRC: %d", crc_received);
-    ESP_LOGI(TAG, "Calculated CRC: %d", crc_calculated); 
-    if (crc_received != crc_calculated) {
-        ESP_LOGW(TAG, "CRC mismatch");
-        return;
-    }
-      // Извлекаем длину полезной нагрузки
-    uint16_t payload_len = (data[4] << 8) | data[5];
-    
-  
-
-}
-
-// Периодическая отправка регулярных запросов
-void periodic_request_task(void *pvParameters) {
-    uart_command_t cmd;
-    while (1) {
-        cmd.cmd_type = 0;
-        cmd.command = COMMAND_REGULAR_REQUEST;
-        cmd.payload_len = 0;
-
-        // Отправка регулярного запроса в очередь команд
-        if (xQueueSend(uart_command_queue, &cmd, 0) != pdPASS) {
-            ESP_LOGW(TAG, "Failed to send regular request");
-        }
-        vTaskDelay(pdMS_TO_TICKS(1000));  // Периодическое ожидание 1 секунду
-    }
-}
-
-// Задача для отправки команд через UART
-void uart_command_task(void *pvParameters) {
-    uart_command_t cmd;
-    uint8_t request[64];
-    uint16_t request_len;
-
-    while (1) {
-        // Ожидаем запрос из очереди
-        if (xQueueReceive(uart_command_queue, &cmd, portMAX_DELAY) == pdPASS) {
-            // Формируем запрос
-            request_len = form_tuya_request(cmd.cmd_type, cmd.command, cmd.payload, cmd.payload_len, request);
-
-            // Логируем запрос
-            ESP_LOGI(TAG, "Sending command %d via UART, request length: %d", cmd.command, request_len);
-            ESP_LOG_BUFFER_HEX(TAG, request, request_len);
-
-
-           size_t buffered_size;
-				uart_get_buffered_data_len(UART_NUM_1, &buffered_size);
-				ESP_LOGI(TAG, "Buffered data size: %d", buffered_size);
-				
-				if (buffered_size > 256) {
-				    ESP_LOGW(TAG, "Buffer overflow: %d bytes buffered", buffered_size);
-				    uart_flush_input(UART_NUM_1);  // Сброс буфера
-				}
-
-            // Используем мьютекс для доступа к UART
-            if (xSemaphoreTake(uart_mutex, pdMS_TO_TICKS(50))) {
-                int res = uart_write_bytes(UART_NUM_1, (const char *)request, request_len);
-                if (res < 0) {
-                    ESP_LOGE(TAG, "Failed to write bytes to UART");
-                }
-                xSemaphoreGive(uart_mutex);  // Освобождаем доступ к UART
-                  uart_flush_input(UART_NUM_1); 
-            } else {
-                ESP_LOGE(TAG, "Failed to take UART mutex");
-            }
-        }
-    }
-}
 
 void init_nvs() {
     ESP_LOGI("NVS", "Initializing NVS...");
@@ -1652,22 +1403,16 @@ void app_main(void) {
         ESP_LOGE(TAG, "Failed to create UART mutex");
         return;  // Обработка ошибки
     }
- 
+   uart_command_queue = xQueueCreate(10, sizeof(uart_command_t));
    init_uart();
    
 	 // Создание очередей
-    uart_command_queue = xQueueCreate(10, sizeof(uart_command_t));
-   // uart_event_queue = xQueueCreate(10, sizeof(uart_event_t));
-//		uart_event_queue = xQueueCreate(20, sizeof(uart_event_t));  
-	//	if (uart_event_queue == NULL) {
-//		    ESP_LOGE(TAG, "Failed to create UART event queue");
-	//	}
+    //uart_command_queue = xQueueCreate(10, sizeof(uart_command_t));
 		// Создаём задачу для обработки UART событий
     xTaskCreate(uart_event_task, "uart_event_task", 2048, NULL, 12, NULL);
 
     // Создание задач
     xTaskCreate(wifi_signal_strength_task, "wifi_signal_strength_task", 2048, NULL, 5, NULL);
-  //  xTaskCreate(uart_event_task, "uart_event_task", 2048, NULL, 10, NULL);
     xTaskCreate(uart_command_task, "uart_command_task", 2048, NULL, 5, NULL);
     xTaskCreate(periodic_request_task, "periodic_request_task", 2048, NULL, 5, NULL);
       // Запуск задачи для отслеживания состояния пина заводских настроек
---
+++ Web_Server/main/uart.h
@@ -0,0 +1,50 @@
+
+#ifndef MAIN_UART_H_
+#define MAIN_UART_H_
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/queue.h"
+#include "driver/uart.h"
+#include "freertos/semphr.h"
+#include "driver/gpio.h"
+#include <stdint.h>
+// Определите пины для UART
+#define TXD_PIN (GPIO_NUM_10)  // Передача данных (TX)
+#define RXD_PIN (GPIO_NUM_9)  // Приём данных (RX)
+#define RTS_PIN (UART_PIN_NO_CHANGE)  // Управление передачей данных (опционально)
+
+#define UART_PORT_NUM      UART_NUM_1
+#define UART_BAUD_RATE     57600
+#define UART_BUF_SIZE      (1024)
+#define RX_BUFFER_SIZE 32
+#define HEADER_1 0x55
+#define HEADER_2 0xAA
+
+// Определяем командные коды
+#define COMMAND_REGULAR_REQUEST 0x01  // Команда для регулярного запроса данных
+#define COMMAND_FRONTEND 0x02         // Команда от фронтэнда
+//#define TAG "UART"
+#define QUEUE_SIZE 10
+
+// Структура для передачи данных через очередь
+typedef struct {
+    uint8_t cmd_type;
+    uint8_t command;
+    uint8_t payload[10];  // Полезная нагрузка, максимальная длина 10 байт
+    uint8_t payload_len;
+} uart_command_t;
+
+extern QueueHandle_t uart_command_queue;
+extern SemaphoreHandle_t uart_mutex;
+//extern uint8_t rx_buffer[RX_BUFFER_SIZE];
+
+// Функции для работы с UART
+void init_uart(void);
+void process_received_data(uint8_t *data, int len);
+uint16_t calcCRC16(uint8_t *buffer, uint8_t u8length);
+uint16_t form_tuya_request(uint8_t cmd_type, uint8_t command, uint8_t *payload, uint16_t payload_len, uint8_t *request);
+static void IRAM_ATTR uart_isr(void* arg);
+void periodic_request_task(void *pvParameters);
+void uart_command_task(void *pvParameters);
+void uart_event_task(void *pvParameters);
+#endif /* MAIN_UART_H_ */
